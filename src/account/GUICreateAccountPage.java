33477060322b1294dd35f003a8b063addf8b6d8d
a <a>
2022-07-30 00:01:16 -0700

    GUICreateAccountPage reading images

--- src/account/GUICreateAccountPage.java
+++ src/account/GUICreateAccountPage.java
@@ -1,16 +1,20 @@
 package account;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class GUICreateAccountPage extends JPanel {
     public static final int TEXT_FIELD_X = 117;
     public static final int TEXT_FIELD_W = 265;
     public static final int TEXT_FIELD_H = 20;
     public static final int BUTTON_X = 117;
-    public static final int BUTTON_Y = 117;
-    public static final int BUTTON_W = 265;
-    public static final int BUTTON_H = 20;
+    public static final int BUTTON_Y = 448;
+    public static final int BUTTON_W = 254;
+    public static final int BUTTON_H = 40;
+
+    public BufferedImage signUpbtnImage;
     public JTextField tfName;
     public JTextField tfEmail;
     public JTextField tfPassword;
@@ -21,6 +25,15 @@ public class GUICreateAccountPage extends JPanel {
 //        this.setBackground(new Color(245,225,195));
         this.setBounds(0, 0, 500, 700);
         setLayout(null);
+
+        //read images
+//        try { // offline button image
+//            signUpbtnImage = ImageIO.read(new File(OFFLINE_BUTTON_IMAGE_FILENAME));
+//        } catch (IOException e) {
+//            System.out.println("LoginPanel::LoginPanel(...): error: file not found: " + OFFLINE_BUTTON_IMAGE_FILENAME);
+//            System.exit(1);
+//        }
+
         tfName = new JTextField("Full Name");
         tfName.setBounds(TEXT_FIELD_X, 250, TEXT_FIELD_W, TEXT_FIELD_H);
 
@@ -33,8 +46,8 @@ public class GUICreateAccountPage extends JPanel {
         tfDisplayName = new JTextField("Display Name");
         tfDisplayName.setBounds(TEXT_FIELD_X, 393, TEXT_FIELD_W, TEXT_FIELD_H);
 
-        btnSignUp = new JButton("Sign Up");
-        btnSignUp.setBounds(TEXT_FIELD_X, 448, TEXT_FIELD_W, TEXT_FIELD_H);
+//        btnSignUp = new JButton(new ImageIcon(loginButtonImage));
+//        btnSignUp.setBounds(BUTTON_X, BUTTON_Y, BUTTON_W, BUTTON_H);
         
     }
 
