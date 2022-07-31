944c6ea1e6f066b27eb4acf0620c8bb7d50fff4a
a <a>
2022-07-30 18:37:51 -0700

    GUICreateAccountPage ui for create account done

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import account.GUICreateAccountPage;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static JFrame frame;
+    public static GUICreateAccountPage guiCreateAccountPage;
+
+    public static void main(String[] args) {
+        frame = new JFrame("CPSC 304 Group 44 Project");
+        frame.setLayout(null);
+        frame.setBackground(Color.white);
+        ImageIcon img = new ImageIcon("frameIcon.GIF");
+        frame.setIconImage(img.getImage());
+        frame.setSize(GUICreateAccountPage.W, GUICreateAccountPage.H);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        guiCreateAccountPage = new GUICreateAccountPage();
+        frame.add(guiCreateAccountPage, new Integer(0), 0);
+        guiCreateAccountPage.setVisible(true);
+
+        frame.setVisible(true);
+    }
+}
--- src/account/GUICreateAccountPage.java
+++ src/account/GUICreateAccountPage.java
@@ -4,17 +4,42 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static java.awt.Font.BOLD;
 
 public class GUICreateAccountPage extends JPanel {
-    public static final int TEXT_FIELD_X = 117;
-    public static final int TEXT_FIELD_W = 265;
-    public static final int TEXT_FIELD_H = 20;
-    public static final int BUTTON_X = 117;
+    Graphics g = null;
+    public static final String SIGN_UP_BTN_IMAGE_FILENAME = "images/sign_up_btn.png";
+    public static final String BORDER_IMAGE_FILENAME = "images/sign_up_border.png";
+    public static final int W = 500;
+    public static final int H = 700;
+
+    public static final int LBL_X = 112;
+    public static final int LBL_Y = 115;
+    public static final int LBL_W = 272;
+    public static final int LBL_H = 32;
+    public static final int BORDER_X = 53;
+    public static final int BORDER_Y = 55;
+    public static final int TEXT_FIELD_X = 112;
+    public static final int TEXT_FIELD_W = 280;
+    public static final int TEXT_FIELD_H = 40;
+
+    public static final int TEXT_FIELD_MARGIN_TOP = 50;
+    public static final int TEXT_FIELD_FULL_NAME_Y = 210;
+    public static final int TEXT_FIELD_EMAIL_Y = TEXT_FIELD_FULL_NAME_Y + TEXT_FIELD_MARGIN_TOP;
+    public static final int TEXT_FIELD_PASSWORD_Y = TEXT_FIELD_EMAIL_Y + TEXT_FIELD_MARGIN_TOP;
+    public static final int TEXT_FIELD_DISPLAY_NAME_Y = TEXT_FIELD_PASSWORD_Y + TEXT_FIELD_MARGIN_TOP;
+
+    public static final int BUTTON_X = 123;
     public static final int BUTTON_Y = 448;
     public static final int BUTTON_W = 254;
     public static final int BUTTON_H = 40;
 
     public BufferedImage signUpbtnImage;
+    public BufferedImage borderImage;
+    public JLabel lblCreateAccount;
     public JTextField tfName;
     public JTextField tfEmail;
     public JTextField tfPassword;
@@ -22,36 +47,61 @@ public class GUICreateAccountPage extends JPanel {
     public JButton btnSignUp;
 
     public GUICreateAccountPage() {
-//        this.setBackground(new Color(245,225,195));
-        this.setBounds(0, 0, 500, 700);
         setLayout(null);
+        this.setBackground(new Color(0,255,255));
+        this.setBounds(0, 0, 500, 700);
 
-        //read images
-//        try { // offline button image
-//            signUpbtnImage = ImageIO.read(new File(OFFLINE_BUTTON_IMAGE_FILENAME));
-//        } catch (IOException e) {
-//            System.out.println("LoginPanel::LoginPanel(...): error: file not found: " + OFFLINE_BUTTON_IMAGE_FILENAME);
-//            System.exit(1);
-//        }
+        //---------------------------------------------------------------------
+        // read images
+        //---------------------------------------------------------------------
+        try { // signUp button image
+            signUpbtnImage = ImageIO.read(new File(SIGN_UP_BTN_IMAGE_FILENAME));
+        } catch (IOException e) {
+            System.out.println("GUICreateAccountPage::GUICreateAccountPage(): error: file not found: " + SIGN_UP_BTN_IMAGE_FILENAME);
+            System.exit(1);
+        }
+
+        try { // border image
+            borderImage = ImageIO.read(new File(BORDER_IMAGE_FILENAME));
+        } catch (IOException e) {
+            System.out.println("GUICreateAccountPage::GUICreateAccountPage(): error: file not found: " + BORDER_IMAGE_FILENAME);
+            System.exit(1);
+        }
+
+        //---------------------------------------------------------------------
+        // init JComponents
+        //---------------------------------------------------------------------
+        lblCreateAccount = new JLabel("CREATE ACCOUNT");
+        lblCreateAccount.setBounds(LBL_X, LBL_Y, LBL_W, LBL_H);
+        lblCreateAccount.setFont(new Font("Arial", BOLD, 28));
+        this.add(lblCreateAccount);
 
         tfName = new JTextField("Full Name");
-        tfName.setBounds(TEXT_FIELD_X, 250, TEXT_FIELD_W, TEXT_FIELD_H);
+        tfName.setBounds(TEXT_FIELD_X, TEXT_FIELD_FULL_NAME_Y, TEXT_FIELD_W, TEXT_FIELD_H);
+        this.add(tfName);
 
         tfEmail = new JTextField("Email Address");
-        tfEmail.setBounds(TEXT_FIELD_X, 297, TEXT_FIELD_W, TEXT_FIELD_H);
+        tfEmail.setBounds(TEXT_FIELD_X, TEXT_FIELD_EMAIL_Y, TEXT_FIELD_W, TEXT_FIELD_H);
+        this.add(tfEmail);
 
         tfPassword = new JTextField("Password");
-        tfPassword.setBounds(TEXT_FIELD_X, 345, TEXT_FIELD_W, TEXT_FIELD_H);
+        tfPassword.setBounds(TEXT_FIELD_X, TEXT_FIELD_PASSWORD_Y, TEXT_FIELD_W, TEXT_FIELD_H);
+        this.add(tfPassword);
 
         tfDisplayName = new JTextField("Display Name");
-        tfDisplayName.setBounds(TEXT_FIELD_X, 393, TEXT_FIELD_W, TEXT_FIELD_H);
+        tfDisplayName.setBounds(TEXT_FIELD_X, TEXT_FIELD_DISPLAY_NAME_Y, TEXT_FIELD_W, TEXT_FIELD_H);
+        this.add(tfDisplayName);
 
-//        btnSignUp = new JButton(new ImageIcon(loginButtonImage));
-//        btnSignUp.setBounds(BUTTON_X, BUTTON_Y, BUTTON_W, BUTTON_H);
-        
+        btnSignUp = new JButton(new ImageIcon(signUpbtnImage));
+        btnSignUp.setBounds(BUTTON_X, BUTTON_Y, BUTTON_W, BUTTON_H);
+        this.add(btnSignUp);
+
+        repaint();
     }
 
     public void paint(Graphics g) {
-
+        System.out.println("oop");
+        g.drawImage(borderImage, BORDER_X, BORDER_Y, null);
+        paintComponents(g);
     }
 }
