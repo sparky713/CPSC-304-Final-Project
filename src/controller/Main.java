bced32b363f3d267744dd3343662524dbb41fbe2
a <a>
2022-08-03 17:30:00 -0700

    Manually Merged 'sally' into 'main'

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -3,6 +3,7 @@ package controller;
 import database.DatabaseConnectionHandler;
 import model.Character;
 import model.ElementModel;
+import ui.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,7 +12,14 @@ import java.util.TimerTask;
 import java.util.*;
 
 public class Main {
-
+    //    public static DatabaseConnectionHandler dbHandler = null;
+    public static JFrame frame;
+    public static GUIMainPage guiMainPage;
+    public static GUICreateAccountPage guiCreateAccountPage;
+    public static GUICharactersPage guiCharactersPage;
+    public static GUIPartiesPage guiPartiesPage;
+    public static GUIWeaponsPage guiWeaponsPage;
+    public static GUIAbilitiesPage guiAbilitiesPage;
     public static void main(String[] args) {
 
         DatabaseConnectionHandler dbHandler = new DatabaseConnectionHandler();
@@ -29,5 +37,50 @@ public class Main {
 
         dbHandler.levelCharacter("Qiqi", 5);
 
+    }
+
+    public static void changeScreen(int screenNum) {
+        if (screenNum == 1) { //sign-up page
+            if (guiMainPage.isVisible()) {
+                guiMainPage.setVisible(false);
+            }
+            frame.setSize(GUICreateAccountPage.W, GUICreateAccountPage.H);
+            guiCreateAccountPage.setVisible(true);
+        }
+        else if (screenNum == 2) {
+            if (guiCreateAccountPage.isVisible()) {
+                guiCreateAccountPage.setVisible(false);
+            }
+            frame.setSize(GUIMainPage.W, GUIMainPage.H);
+            guiMainPage.setVisible(true);
+        }
+//        else if (screenNum == 3) {
+//            if (guiMainPage.isVisible()) {
+//                guiMainPage.setVisible(false);
+//            }
+//            guiCharactersPage.setVisible(true);
+//        }
+//        else if (screenNum == 4) {
+//            if (guiMainPage.isVisible()) {
+//                guiMainPage.setVisible(false);
+//            }
+//            guiPartiesPage.setVisible(true);
+//        }
+        else if (screenNum == 5) {
+            if (guiMainPage.isVisible()) {
+                guiMainPage.setVisible(false);
+            }
+            guiWeaponsPage.setVisible(true);
+        }
+        else if (screenNum == 6) {
+            if (guiMainPage.isVisible()) {
+                guiMainPage.setVisible(false);
+            }
+            guiAbilitiesPage.setVisible(true);
+        }
+        else {
+//            ERROR MESSAGE!!!!!!
+            System.out.println("Game::changeScreen(" + screenNum + "): Error. Page Not Found");
+        }
     }
 }
