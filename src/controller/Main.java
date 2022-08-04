b5530f7e37c8674a21187050eb856e292101e93f
Jasmine <jmann507@students.cs.ubc.ca>
2022-08-03 17:32:34 -0700

    created test player, food, and made the player consume 2 amounts of the food

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -3,6 +3,8 @@ package controller;
 import database.DatabaseConnectionHandler;
 import model.Character;
 import model.ElementModel;
+import model.Food;
+import model.Player;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,7 +17,14 @@ public class Main {
     public static void main(String[] args) {
 
         DatabaseConnectionHandler dbHandler = new DatabaseConnectionHandler();
-        dbHandler.login("ORA", "a");
+        dbHandler.login("ORA_jmann507", "???");
+
+        //creating test player
+        Player player1 = new Player("player1", "password123", "player1@gmail.com", "Tiger123");
+
+        //adding food
+        Food mushroomPizza = new Food("Mushroom Pizza", 450);
+
 
         ElementModel cryo = new ElementModel("Cryo");
         Character qiqi = new Character("Qiqi", cryo);
@@ -23,11 +32,17 @@ public class Main {
         qiqi.setBaseHP(100);
         qiqi.setLevel(0);
 
+        player1.consumes(mushroomPizza, 2);
+
         dbHandler.databaseSetup();
         dbHandler.insertElement(cryo);
         dbHandler.insertCharacter(qiqi);
 
+        dbHandler.insertFood(mushroomPizza);
+        dbHandler.insertConsumes(player1, mushroomPizza, 2);
+
         dbHandler.levelCharacter("Qiqi", 5);
+
 
     }
 }
