f4488fde615d05eceb893854567d566fc9120a56
Jasmine <jmann507@students.cs.ubc.ca>
2022-08-05 15:22:26 -0700

    implemented "consumes" method in Player class and added a HashMap of player's food inventory

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,21 +1,28 @@
 package model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Player {
-    private final String userName;
+    private final String username;
     private final String email;
     private final String password;
     private final String displayName;
+    private Map<String,Integer> foodInventory;
 
-    public Player(String userName, String email, String password, String displayName) {
-        this.userName = userName;
+
+    public Player(String username, String email, String password, String displayName) {
+        this.username = username;
         this.email = email;
         this.password = password;
         this.displayName = displayName;
+        // Hashmap "food" stores the names of food the player owns, as well as each corresponding quantity
+        this.foodInventory = new HashMap<String,Integer>();
     }
 
     // getters
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
     public String getEmail() {
@@ -29,4 +36,15 @@ public class Player {
     public String getDisplayName() {
         return displayName;
     }
+
+    public void consumes(Food food, int amount){
+        String foodName = food.getFoodName();
+        if (foodInventory.containsKey(foodName)){
+            int currQuantity = foodInventory.get(foodName);
+            foodInventory.put(foodName, currQuantity + amount);
+        } else {
+            foodInventory.put(foodName, amount);
+        }
+    }
+
 }
